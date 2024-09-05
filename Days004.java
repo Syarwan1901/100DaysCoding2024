a1be4bdaeaa00e422b7d1b028d17897f8722d96e
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-05 16:33:23 +0800

    Create Days004.java

---
+++ Days004.java
@@ -0,0 +1,15 @@
+package isc004;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String nim ="D024323";
+        char kelas ='C';
+        
+        System.out.print("NAMA     :ABDULLAH SYARWAN\n");
+        System.out.printf("NIM     : %s \n", nim);
+        System.out.printf("KELAS   : %c \n", kelas);
+    }
+    
+}
