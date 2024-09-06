ac623711d15a85d9b5275560963ad83569409ffa
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-06 18:05:24 +0800

    Create Days005.java

---
+++ Days005.java
@@ -0,0 +1,27 @@
+package iscDays005;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        // untuk variable
+        
+        String nim ="D024323";
+        char kelas ='C';
+        /* untuk print
+         * dan untuk printf
+         */
+        
+         
+        System.out.print("NAMA     :ABDULLAH SYARWAN\n");
+        System.out.printf("NIM     : %s \n", nim);
+        System.out.printf("KELAS   : %c \n", kelas);
+    }
+    
+
+
+    
+
+    }
+
