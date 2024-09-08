78360d91ec1a6ccbe7350ad3024761e4b5def42a
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-08 19:43:45 +0800

    Create Days007.java

---
+++ Days007.java
@@ -0,0 +1,15 @@
+package Days7Isc;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String nama ="iyan";
+        
+        
+        
+        
+        System.out.println("nama: " + nama);
+    }
+    
+}
