9e48c7f8ead10e1e4409c029a28b44b55630841d
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-09 13:42:49 +0800

    Create Days008.java

---
+++ Days008.java
@@ -0,0 +1,16 @@
+package iscdays8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // variable update
+        String nama ="abdullah";
+        System.out.println(nama);
+        nama = "syarwan";
+        System.out.println(nama);
+        
+        
+       
+    }
+    
+}
