ebbe16d5bb17122652cd3719e006dcc62c4611de
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-10 17:05:07 +0800

    Update Days009.java

--- Days009.java
+++ Days009.java
@@ -8,7 +8,7 @@ public class Main {
         // final variable
         final byte umur= 18;
           System.out.println("umur\t : " + umur);
-        final float semangat= 999f;
+        final float semangat= 999,f;
           System.out.print("semangat : " + semangat);
         
         
