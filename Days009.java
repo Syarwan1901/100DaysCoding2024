d8c5daf5aa4bbeb8d2d9f882491043bb3224a8f9
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-10 17:00:20 +0800

    Create Days009.java

---
+++ Days009.java
@@ -0,0 +1,17 @@
+package Days009;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        // final variable
+        final byte umur= 18;
+          System.out.println("umur\t : " + umur);
+        final float semangat= 999f;
+          System.out.print("semangat : " + semangat);
+        
+        
+    }
+    
+}
