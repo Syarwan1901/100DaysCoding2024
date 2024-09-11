b831d69dd3266c65d91b94e3e8d2c668ca8a7974
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-11 21:27:37 +0800

    Create Days010.java

---
+++ Days010.java
@@ -0,0 +1,12 @@
+package Isc010;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        byte angka =110;
+        
+        System.out.println("angka " + angka);
+        
+    }
+    
+}
