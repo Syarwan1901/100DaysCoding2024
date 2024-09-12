42a09c710fed355b1d96dd3784a0ce83d4c6ca83
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-12 21:19:13 +0800

    Create Days011.java

---
+++ Days011.java
@@ -0,0 +1,11 @@
+package iscDays11;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        short hasil =132;
+        System.out.println("hasil " + hasil);
+    }
+    
+}
