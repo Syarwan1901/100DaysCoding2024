1f519ac0afc649dc99261f63f3515ca0b00b229b
Syarwan1901 <abdullahsyarwan8@gmail.com>
2024-09-13 15:52:18 +0800

    Create Days012.java

---
+++ Days012.java
@@ -0,0 +1,10 @@
+package iscdays012;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int jumlahnya =135;
+        System.out.println("jumlahnya : " + jumlahnya  );
+    }
+    
+}
